1120b3896250b9076036e8af5cfd5778a7692874
yliu098 <liuyuzhe70412@gmail.com>
2026-04-22 14:14:53 -0400

    Initial commit

---
+++ print('welcome to python').py
@@ -0,0 +1,22 @@
+print('welcome to python')
+print('Per Scholas is Unlocking Potential and Changing the Face of Tech')
+
+print(10+90)
+
+print(300.5 + 0.5)
+
+# sep= attribute/parameter puts a separator of choice betwenn the comma separated value
+print (12, 24, -2, sep=':')
+print('but', 'not', 'including', sep='**')
+print('but', 'not', 'including', sep=' ')
+
+# Saving response into variable
+name = input('What is your name?')
+print('Hello', name, 'what is up?')
+
+# Get numbers from the user
+mb = input('Enter your mobile num:')
+hp = input('Enter your home number:')
+
+print('Mobile number is ', mb)
+print('Home number is ', hp)
