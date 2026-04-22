91e98fa4c163086f1783ad55b9b899510415eb77
yliu098 <liuyuzhe70412@gmail.com>
2026-04-22 14:23:51 -0400

    Initial commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+# This is a comment. It is ignored by the Python interpreter.
+# Comments are used to explain the code and make it easier to read.
+
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+# The input() function is used to prompt the user for input.
+# Whatever the user enters is returned as a string, and is stored in the variables "name" and "age".
+
+print("Hello, " + name + "!")
+print("You are " + age + " years old.")
+
+# The print() function is used to generate output. It can be used to print strings, variables, and more.
+# The "+" operator is used to concatenate (join) strings together.
